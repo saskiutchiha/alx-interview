e22653ebae2751b7468354f7feb48bc072b9385e
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-04-25 13:04:02 +0100

    Create 0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def trinagle(n):
+    l = [[1]]
+    if n <= 0 :
+        return []
+
+    for i in range(1,n+1):
+        l = l + [l[i-1] + [1]]
+        j = i-1
+        while j > 0:
+            l[i][j] = l[i][j] + l[i][j-1]
+            j-=1
+    return l
