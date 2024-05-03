8d66c817f761f9b423807d4d9991cdb3cd3853ba
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-05-03 02:23:21 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,11 +1,34 @@
 #!/usr/bin/python3
+
+"""Solves the lock boxes puzzle """
+
 def unlock(L,li,indice =0):
+  """
+  This function is used to unlock the boxes.
+    
+  Args:
+        L (list): List of boxes with keys.
+        li (list): List of boxes status, 1 indicates the box is unlocked, 0 indicates the box is locked.
+        indice (int, optional): The index of the box to start with. Defaults to 0.
+        
+  Returns:
+    None
+  """
   if L[indice] != [] :
       for key in L[indice]:
        if li[key] != 1:
         li[key] = 1
         unlock(L,li,key)
 def canUnlockAll(L):
+ """
+ This function checks if all boxes can be unlocked.
+    
+ Args:
+    L (list): List of boxes with keys.
+        
+ Returns:
+        bool: True if all boxes can be unlocked, otherwise False.
+ """
  li = [1] + [0]*(len(L)-1)
  unlock(L,li)
  if 0 in li :
