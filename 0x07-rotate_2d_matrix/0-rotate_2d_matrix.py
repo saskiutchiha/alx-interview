613f9212cee85176990124ce2e6d88e0c34db676
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-06-11 02:08:57 +0100

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,1 +1,7 @@
-ww
+import copy as cp
+def rotate_2d_matrix(matrix):
+    A = cp.deepcopy(matrix)
+    
+    for i in range(len(A)):
+        for k in range(len(A[i])):
+          L[k][-i-1] = A[i][k]
