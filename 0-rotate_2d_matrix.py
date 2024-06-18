cd01908152167c31b92afe4c0ea1acebdcee26e5
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-06-18 14:21:19 +0100

    Update 0-rotate_2d_matrix.py

--- 0-rotate_2d_matrix.py
+++ 0-rotate_2d_matrix.py
@@ -1,13 +1,1 @@
-#!/usr/bin/python3
-"""2D matrix rotation module.
-"""
 
-def dd_matrix(L):
-    A = [i[:] for i in L]
-    
-    for i in range(len(A)):
-        for k in range(len(A[i])):
-          L[k][-i-1] = A[i][k]
-L = [[1,2,3],[4,5,6],[7,8,9]]
-dd_matrix(L)
-print(L)
