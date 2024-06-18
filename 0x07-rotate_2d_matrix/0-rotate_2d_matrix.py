dadc487cbe10dfcb6d8e2774e0e015330368ad5d
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-06-18 14:29:23 +0100

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""2D matrix rotation module.
+"""
+2D matrix rotation module.
 """
 
 def rotate_2d_matrix(L):
-     """Rotates an m by n 2D matrix in place.
+    """Rotates an m by n 2D matrix in place.
     """
     A = [i[:] for i in L]
     
