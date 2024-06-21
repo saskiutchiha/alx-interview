127030f185790726be9a359ff980445c5e74f351
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-06-21 18:27:49 +0100

    Create 0-validate_utf8.py

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""UTF-8 validation module.
+"""
+
+def validUTF8(data):
+    """Checks if a list of integers are valid UTF-8 codepoints.
+    """
+    for number in data :
+        if len(bin(number)[2:]) >= 8:
+           return  False
+    return True
+print(validUTF8([65]))
