0898f985f89adab1de53b3098e955a9cd4432edc
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-06-28 01:46:02 +0100

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,52 +9,7 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
          if grid[i][j] == 1:
-            if i == 0 or j == 0 or i == len(grid) or j == len(grid[i]) : 
-                if j == 0 :
-                    if i == 0:
-                     if grid[i+1][j] == 0:
-                        per = per + 1
-                    elif i == len(grid) -1:
-                        if grid[i-1][j] == 0:
-                         per = per + 1
-                    else :
-                       if grid[i+1][j] == 0:
-                        per = per + 1
-                       if grid[i-1][j] == 0:
-                         per = per + 1
-                    if grid[i][j+1] == 0:
-                        per = per + 1
-                       
-                elif j == len(grid[i]-1):
-                    if i == 0 :
-                       if grid[i+1][j] == 0:
-                        per = per + 1
-                    if i == len(grid) -1:
-                      if grid[i+1][j] == 0:
-                        per = per + 1  
-                    else :
-                       if grid[i+1][j] == 0:
-                        per = per + 1
-                       if grid[i-1][j] == 0:
-                         per = per + 1
-                    if grid[i][j-1] == 0:
-                        per = per + 1
-                elif i == 0 :
-                   if grid[i][j-1] == 0:
-                        per = per + 1
-                   if grid[i][j+1] == 0:
-                        per = per + 1
-                   if grid[i+1][j] == 0:
-                        per = per + 1
-                elif i ==  en(grid[i]-1):
-                   if grid[i][j-1] == 0:
-                        per = per + 1
-                   if grid[i][j+1] == 0:
-                        per = per + 1
-                   if grid[i-1][j] == 0:
-                        per = per + 1
-                
-            else :
+            
                 if grid[i-1][j] == 0:
                     per+=1
                 if grid[i][j-1] == 0:
