6f9a4143bec9b9c90c065ea8c806654de9802668
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-06-28 13:44:13 +0100

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -10,6 +10,62 @@ def island_perimeter(grid):
         for j in range(len(grid[i])):
          if grid[i][j] == 1:
             
+            if i == 0 or j == 0 or i == len(grid)-1 or j == len(grid[i])-1 : 
+                if j == 0 :
+                    if i == 0:
+                     per+=2
+                     if grid[i+1][j] == 0:
+                        per = per + 1
+                    elif i == len(grid) -1:
+                        per+=2
+                        if grid[i-1][j] == 0:
+                         per = per + 1
+                    else :
+                       per+=1
+                       if grid[i+1][j] == 0:
+                        per = per + 1
+                       if grid[i-1][j] == 0:
+                         per = per + 1
+                    if grid[i][j+1] == 0:
+                        per = per + 1
+                    
+                       
+                elif j == len(grid[i]) -1:
+                    if i == 0 :
+                       per+=2
+                       if grid[i+1][j] == 0:
+                        per = per + 1
+                    elif i == len(grid) -1:
+                      per+=2
+                      if grid[i-1][j] == 0:
+                        per = per + 1  
+                    else :
+                       per+=1
+                       if grid[i+1][j] == 0:
+                        per = per + 1
+                       if grid[i-1][j] == 0:
+                         per = per + 1
+                    if grid[i][j-1] == 0:
+                        per = per + 1
+                elif i == 0 :
+                   per+=1
+                   if grid[i][j-1] == 0:
+                        per = per + 1
+                   if grid[i][j+1] == 0:
+                        per = per + 1
+                   if grid[i+1][j] == 0:
+                        per = per + 1
+                elif i ==  len(grid) -1:
+                   per+=1
+                   if grid[i][j-1] == 0:
+                        per = per + 1
+                   if grid[i][j+1] == 0:
+                        per = per + 1
+                   if grid[i-1][j] == 0:
+                        per = per + 1
+                   
+                
+            else :
                 if grid[i-1][j] == 0:
                     per+=1
                 if grid[i][j-1] == 0:
@@ -18,4 +74,6 @@ def island_perimeter(grid):
                     per+=1
                 if grid[i+1][j] == 0:
                     per+=1
+            print(i,j,per)
+            
     return per
